81c4a25172f7f45a3ab8f0a42a39e212fc9a1437
Андрей Малыгин <shapckin5@mail.ru>
2025-01-22 14:55:30 +0700

    Добавьте файлы проекта.

---
+++ 26_Malygin_authorization/Helper.cs
@@ -0,0 +1,28 @@
+﻿using _26_Malygin_authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_Malygin_authorization
+{
+    public class Helper
+    {
+        private static Entities context;
+
+        public static Entities GetContext()
+        {
+            if (context == null)
+            {
+                context = new Entities();
+            }
+            return context;
+        }
+
+        public Helper()
+        {
+            context = GetContext();
+        }
+    }
+}
---
+++ 26_Malygin_authorization/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _26_Malygin_authorization
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                string login = txtbLogin.Text;
+                string password = pswbPassword.Password;
+                var user = Helper.GetContext().Users.Where(x => x.Login == login && x.Password == password).FirstOrDefault();
+                string role = Helper.GetContext().Roles.Where(x => x.roleID == user.roleID).FirstOrDefault().role;
+
+                if (user != null)
+                {
+                    txtbMessage.Text = $"Добро пожаловать! Ваша роль: {role}";
+                }
+                else 
+                {
+                    txtbMessage.Text = "Неверно введены логин или пароль!";
+                }
+            }
+            catch 
+            {
+                txtbMessage.Text = "Неверно введены логин или пароль!";
+            }
+        }
+    }
+}
